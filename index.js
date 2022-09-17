e40358bf46adfb1cb6569c320c63b15a0971d777
roadblock37 <Roadblock37@gmail.com>
2022-09-17 17:48:20 -0400

    toggle now changes with theme change

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
+document.getElementById("theme-toggle").addEventListener("click", handleTheme);
 
-document.getElementById('theme-toggle').addEventListener("click", handleTheme);
-
-function handleTheme(){
-    let checkBox = document.getElementById('theme-toggle');
-    if (checkBox.checked === true){
-        document.documentElement.className = "theme-dark";
-        console.log("theme changed to dark")
-    }
-    else{
-        document.documentElement.className = "theme-light";
-
-    }
+function handleTheme() {
+  let checkBox = document.getElementById("theme-toggle");
+  if (checkBox.checked === true) {
+    document.documentElement.className = "theme-light";
+    console.log("theme changed to light");
+  } else {
+    document.documentElement.className = "theme-dark";
+  }
 }
