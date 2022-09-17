6d73c49522c75a0620a1a0d2819b1a7c35591c6c
roadblock37 <Roadblock37@gmail.com>
2022-09-17 17:53:25 -0400

    toggle title changes on theme change

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 document.getElementById("theme-toggle").addEventListener("click", handleTheme);
 
 function handleTheme() {
+  const label = document.getElementById("theme-label");
   let checkBox = document.getElementById("theme-toggle");
   if (checkBox.checked === true) {
     document.documentElement.className = "theme-light";
+    label.innerText = "Light Mode";
+
     console.log("theme changed to light");
   } else {
     document.documentElement.className = "theme-dark";
+    label.innerText = "Dark Mode";
   }
 }
